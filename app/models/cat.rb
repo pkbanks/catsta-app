b13ab44565a731b5d6d75de1b615aa30b02f0023
pkbanks <pkbanks@gmail.com>
2017-11-03 14:11:32 -0400

    added api call to Cat, require httparty gem, bunded // next step: render view, or update index

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,10 @@
 class Cat < ActiveRecord::Base
-	@@url_base = "http://thecatapi.com/api/"
+
+	@@url_base = "http://thecatapi.com/api/"	
 	api_key = 'MjM5MTgy'
 
+	def self.get_cats
+		url = @@url_base + "/images/get?format=xml&results_per_page=40"
+		return HTTParty.get(url).parsed_response["response"]["data"]["images"]["image"]
+	end
 end
